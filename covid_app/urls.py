8b4bb8c9a5b16fb7aa57f05cb8bc43b857746a74
jashu0022 <jashwanthsai0022@gmail.com>
2023-12-24 22:44:26 +0530

    first commit

---
+++ covid_app/urls.py
@@ -0,0 +1,22 @@
+
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', index, name = 'covid_project'),
+    path('stats', stats, name = 'covid_project'),
+    path('Recovery & Resources', resorse, name= 'covid_project'),
+    path('about', about, name= 'covid_project'),
+    path('contact', contact, name= 'covid_project'),
+    path('privacy', privacy , name = 'covid_project'),
+    path('adminlogin', adminlogin, name='adminlogin'),
+    path('welcome', welcome, name='welcome'),
+    path('contactdet', contactdet, name='contactdet'),
+    path('logout', custom_logout, name='logout'),
+    path('get-country/', get_country )
+
+
+
+]
